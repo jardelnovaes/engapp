dae268c1ad53428e9b4b48e4286ad85b06af5e1b
jardelnovaes <jardelnovaes@hotmail.com>
2016-08-16 11:51:44 -0300

    jardel_xampp

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -5,18 +5,6 @@
 <div class="row">	
 	<?
 	/*
-	{!! Html::ul($errors->all(), array('class'=>'errors')) !!}
-
-	{!! Form::open(array('url' => 'signin','class'=>'form-inline')) !!}
-
-	{!! Form::label('email', 'E-Mail Address') !!}
-	{!! Form::text('email', 'example@gmail .com', array('class' => 'form-control')) !!}
-	{!! Form::label('password', 'Password') !!}
-	{!! Form::password('password', array('class' => 'form-control')) !!}
-
-	{!! Form::submit('Sign In' , array('class' => 'btn btn-primary')) !!}
-	{!! Form::close() !!}
-	
 	See: https://laravel.com/docs/5.1/blade
 	*/
 	?>
